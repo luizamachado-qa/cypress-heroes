71911d29b7d418d6a349000d56a6d9586a95fd9b
luizamachado-qa <luizamachado.ds@gmail.com>
2026-04-15 17:53:09 -0300

    add new hero

--- client/cypress/e2e/heroes.cy.js
+++ client/cypress/e2e/heroes.cy.js
@@ -1,14 +1,27 @@
 describe('Listagem de heróis', () => {
 
-    beforeEach(() => {
-    cy.login('test@test.com', 'test123')
+  beforeEach(() => {
+    cy.login('admin@test.com', 'test123')
     cy.visit('/heroes')
   })
 
   it('Listar herois apos login', () => {
     cy.get("[data-cy='name']").eq('6')
     cy.contains("The Smoker").should('be.visible')
+    cy.get("[data-cy='hero-card']").should('have.length', 7)
   })
 
+  it.only('Criar novo heroi', () => {
+    cy.get("[href='/heroes/new']").click()
+    cy.get("[data-cy='nameInput']").type('CatWoman')
+    cy.get("[data-cy='priceInput']").type('100')
+    cy.get("[data-cy='fansInput']").type('500')
+    cy.get("[data-cy='savesInput']").type('300')
+    cy.get("[data-cy='powersSelect']").select(['Flying', 'Invisibility'])
+    cy.get("[data-cy='avatarFile']").selectFile('cypress/fixtures/avatar.jpg')
+    cy.contains('Button', 'Submit').click()
+    cy.url().should('include', '/heroes')
+    cy.contains('Wonder Woman').should('be.visible')
+  })
 
 })
